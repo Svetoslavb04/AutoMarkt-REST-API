b0f044f717ab0f4d672ed14653a45f868ced95d5
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-12 23:03:15 +0300

    added register method and endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,13 @@ const { register, login } = require('../services/userService');
 router.post('/register', async (req, res) => {
     const { email, username, password} = req.body;
 
-    const user = await register(email, username, password);
+    try {
+        const user = await register(email, username, password);
+        res.json(user);
+    } catch (error) {
+        res.json(error);
+    }
+
 });
 
 router.post('/login', async (req, res) => {
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,17 +1,32 @@
 const User = require('../models/User');
 
-exports.register = async(email, username, password) => {
-    User.create({email, username, password})
-        .then(user => {
-
-        })
+exports.register = async (email, username, password) => {
+    return User.create({ email, username, password })
+        .then(user => { return { email: user.email, username: user.username, _id: user._id } })
         .catch(err => {
-            const errors = err.errors;
+            const error = {};
 
-            const response = {};
+            if (err.name == 'ValidationError') {
+                error.type = 'User Validation Error';
+                error.errors = {};
+
+                const keys = Object.keys(err.errors);
+
+                keys.forEach(key => {
+                    error.errors[key] = err.errors[key].properties.message;
+                });
+
+            } else if (err.name == 'MongoServerError') {
+                error.type = 'Existing email or username';
+            }
+            else {
+                error.type = err.name;
+            }
+
+            throw error;
         })
 }
 
 exports.login = (email, password) => {
-    
+
 }
