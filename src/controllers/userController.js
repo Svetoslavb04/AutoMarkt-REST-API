b833b23db11ea053cc53eb76af9bcbda8ac8782a
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-12 23:34:33 +0300

    added login endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,21 +3,34 @@ const router = require('express').Router();
 const { register, login } = require('../services/userService');
 
 router.post('/register', async (req, res) => {
-    const { email, username, password} = req.body;
+
+    const { email, username, password } = req.body;
 
     try {
+
         const user = await register(email, username, password);
         res.json(user);
-    } catch (error) {
-        res.json(error);
-    }
 
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 router.post('/login', async (req, res) => {
-    const { email, password} = req.body;
 
-    const token = await login(email, password);
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(500).json({ message: 'Email and password are required' })
+    }
+
+    try {
+        const user = await login(email, password);
+        res.json(user);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,15 @@
 const User = require('../models/User');
 
+const jwt = require('jsonwebtoken');
+const secret = require('../config/env-variables.json')[process.env.NODE_ENV].SECRET;
+const bcrypt = require('bcrypt');
+const util = require('util');
+
+const signJWTPromisified = util.promisify(jwt.sign);
+const verifyJWTPromisified = util.promisify(jwt.verify);
+
+
+
 exports.register = async (email, username, password) => {
     return User.create({ email, username, password })
         .then(user => { return { email: user.email, username: user.username, _id: user._id } })
@@ -27,6 +37,43 @@ exports.register = async (email, username, password) => {
         })
 }
 
-exports.login = (email, password) => {
+exports.login = async (email, password) => {
 
+    user = await User.findOne({ email });
+    
+    if (user) {
+
+        const areEqual = await bcrypt.compare(password, user.password);
+
+        if (areEqual) {
+
+            const token = await signJWTPromisified(
+                {
+                    email: user.email,
+                    username: user.username,
+                    _id: user._id
+                }
+                , secret,
+                {
+                  expiresIn: '1d'  
+                });
+
+            return {
+                email: user.email,
+                username: user.username,
+                _id: user._id,
+                xToken: token
+            };
+
+        } else {
+
+            throw {
+                message: "Email or password does not match"
+            }
+        }
+    } else {
+        throw {
+            message: "Email or password does not match"
+        }
+    }
 }
