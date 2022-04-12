182ee00fbb37c76f879922e196b0712f118dadb6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-12 08:37:11 +0300

    set environment variable

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
+const envVariables = require('./config/env-variables.json');
+
 const app = express();
+
+app.listen(
+    envVariables[process.env['NODE_ENV']].PORT,
+    () => console.log(`Server listening on port: ${envVariables[process.env['NODE_ENV']].PORT}`)
+);
