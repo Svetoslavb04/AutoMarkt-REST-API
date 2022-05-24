9939b2a610202d518dcd0f0381c50e10ade54f59
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-24 17:01:24 +0300

    AddCORSOptions

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 const router = require('./src/router');
 
-app.use(require('cors')());
+app.use(require('cors')({
+    origin: process.env.ORIGIN,
+    credentials: true
+}));
+
 app.use(require('cookie-parser')());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
