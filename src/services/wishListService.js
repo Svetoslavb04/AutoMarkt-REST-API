4680639747fbad270d4d1cd38d3e779d817fdb2d
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-31 21:04:20 +0300

    Updated Shopping and WishList services

--- src/services/wishListService.js
+++ src/services/wishListService.js
@@ -4,69 +4,71 @@ exports.get = (owner_id) => WishList.findOne({ owner_id })
     .then(wishList => wishList)
     .catch(err => { return { items: [] } });
 
-    exports.create = async (wishList) => {
+exports.create = async (wishList) => {
 
-        if (wishList.items.length <= 0) {
-    
-            try {
-    
-                const deletedList = await WishList.findOneAndDelete({ owner_id: wishList.owner_id });
-    
-                return deletedList || { owner_id: wishList.owner_id, items: [] };
-                
-            } catch (error) {
-                
-            }
-    
-        }
-    
+    console.log(wishList);
+
+    if (wishList.items?.length <= 0) {
+
         try {
-    
-            const list = await WishList.findOneAndUpdate(
-                { owner_id: wishList.owner_id },
-                { items: wishList.items },
-                {
-                    new: true,
-                    upsert: true
-                }
-            );
-    
-            return list;
-    
-        } catch (err) {
-            
-            const error = {};
-    
-            if (err.name == 'ValidationError') {
-    
-                error.message = 'WishList Validation Error';
-                error.errors = {};
-    
-                const keys = Object.keys(err.errors);
-    
-                keys.forEach(key => {
-    
-                    if (err.errors[key].properties) {
-    
-                        error.errors[key] = err.errors[key].properties.message;
-    
-                    } else {
-    
-                        error.errors[key] = 'Invalid data type';
-    
-                    }
-    
-                });
-    
-            } else {
-    
-                error.message = err.name;
-    
-            }
-    
-            throw error;
+
+            const deletedList = await WishList.findOneAndDelete({ owner_id: wishList.owner_id });
+
+            return deletedList || { owner_id: wishList.owner_id, items: [] };
+
+        } catch (error) {
+
         }
+
     }
+
+    try {
+
+        const list = await WishList.findOneAndUpdate(
+            { owner_id: wishList.owner_id },
+            { items: wishList.items },
+            {
+                new: true,
+                upsert: true
+            }
+        );
+
+        return list;
+
+    } catch (err) {
+
+        const error = {};
+
+        if (err.name == 'ValidationError') {
+
+            error.message = 'WishList Validation Error';
+            error.errors = {};
+
+            const keys = Object.keys(err.errors);
+
+            keys.forEach(key => {
+
+                if (err.errors[key].properties) {
+
+                    error.errors[key] = err.errors[key].properties.message;
+
+                } else {
+
+                    error.errors[key] = 'Invalid data type';
+
+                }
+
+            });
+
+        } else {
+
+            error.message = err.name;
+
+        }
+
+        throw error;
+    }
+}
 
 exports.remove = (owner_id) => WishList.findOneAndDelete({ owner_id })
     .then(() => 'Wish list succesfully removed!')
